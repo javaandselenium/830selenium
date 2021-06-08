6e0c7e01ec13ea7f07c6f2ad669e13600e7b4313
QSP <QSP@QSP>
2021-06-08 10:13:56 +0530

    Selenium Architecture

---
+++ src/basics/Test0.java
@@ -0,0 +1,12 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+
+	}
+
+}

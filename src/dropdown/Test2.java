6695231fbf02a0bdf704ece51611a657555022df
QSP <QSP@QSP>
2021-06-18 10:28:04 +0530

    DropDown

---
+++ src/dropdown/Test2.java
@@ -0,0 +1,39 @@
+package dropdown;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///C:/Users/QSP/Desktop/dd.html");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		WebElement ele = driver.findElement(By.id("MTR"));
+		 Select s=new Select(ele);
+		 s.selectByIndex(2);
+		 s.selectByValue("c");
+		 s.selectByVisibleText("Cake");
+		 
+		
+		 s.deselectByValue("c");
+		
+		 s.deselectByIndex(2);
+		 
+		 s.deselectByVisibleText("Cake");
+		 
+		 driver.close();
+		
+		
+		
+
+	}
+
+}
